af65c77650617ccdd5d192c2a87c7176c22be0fc
Mason Benzinger <mason.benzinger@gmail.com>
2022-08-14 16:29:23 -0500

    added show route books/:id

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -2,8 +2,25 @@ const express = require("express");
 const books = express.Router();
 const Book = require("../models/books.js");
 
-notes.get("/books", async (req, res) => {
-  res.send('get books')
+//root
+books.get("/books", async (req, res) => {
+  Book.find()
+        .then(foundBook => {
+            res.json(foundBook)
+        })
 });
+
+// SHOW
+books.get('/books/:id', (req, res) => {
+  Book.findById(req.params.id)
+       .then(foundBook => {
+        res.render('show', {
+            book: foundBook
+        })
+      })
+      .catch(err => {
+        res.send('404')
+      })
+})
   
   module.exports = books;
